c16c8086f075f3a1bd10b148ac6d92c140938884
Hanus Michal (240621) <240621@vutbr.cz>
2023-11-30 12:53:07 +0100

    servo knihovna a cas

--- servo/lib/servo/servo.h
+++ servo/lib/servo/servo.h
@@ -1,9 +1,20 @@
 #ifndef SERVO_H
 #define SERVO_H
 
-#include "timer.h"
+#include <timer.h>
+#include <gpio.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-void servo_init(volatile uint8_t *reg, uint8_t pin);
-void servo_set_value(uint8_t value);
+typedef struct
+{
+    volatile uint8_t *reg;
+    uint8_t pin;
+    uint8_t value;
+} servo_t;
+
+void servo_init(servo_t *servo, volatile uint8_t *reg, uint8_t pin);
+void servo_set_value(servo_t *servo, uint8_t value);
+void servo_tim0_interrupt_handler(servo_t *servo);
 
 #endif
--- servo/src/main.c
+++ servo/src/main.c
@@ -1,7 +1,16 @@
+#undef F_CPU
+#ifndef F_CPU
+#define F_CPU 16000000 // CPU frequency in Hz required for UART_BAUD_SELECT
+#endif
+
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include "timer.h"
+#include <time.h>
+#include <servo.h>
+#include <uart.h>
 
 /*typedef struct data_t
 {
@@ -12,25 +21,50 @@
 	uint8_t water;
 };*/
 
+servo_t water_servo;
+
 int pos=0;
 
 int main(void)
 {
-	//GPIO_mode_output(&DDRB, 5);
-	DDRB |= (1 << 0); // set PB0 output
-	PORTB &= !(1<<0);
-
-	TIM0_OVF_128US();
-	TIM0_OVF_ENABLE();
-
 	sei();
+	uart_init(UART_BAUD_SELECT(9600, F_CPU));
+
+
+	time_t t = 1701343534;
+	struct tm* ptr;
+
+
+	//set_system_time(t);
+	//set_zone(+1*ONE_HOUR);
+	char str[100];
+
+	t = 1701344319 - 946681200;
+	while(1)
+	{
+		//ptr = localtime(&t);
+		uart_puts(ctime(&t));
+
+		//strftime(str, 100, "Ted je %d.%m.%Y a %H:%M:%D den je %A", &t);
+		//uart_putc(str);
+		uart_puts("\n");
+		//_delay_ms(1000);
+		t++;
+		for(long i=0; i < 0x2ffff0; i++) asm("NOP");
+	}
+
+
+	GPIO_mode_output(&DDRB, 0);
+	servo_init(&water_servo, &PORTB, 0);
 
 	while(1)
-	{//16-44
-		pos=42;
-		_delay_ms(1000);
-		pos=43;
-		_delay_ms(1000);
+	{
+		//GPIO_write(&PORTB, 5, 0);
+		servo_set_value(&water_servo, 0);
+		for(long i=0; i < 0x2ffff0; i++) asm("NOP");
+		//GPIO_write(&PORTB, 5, 1);
+		servo_set_value(&water_servo, 90);
+		for(long i=0; i < 0x2ffff0; i++) asm("NOP");
 	}
 
 	return 0;
@@ -40,12 +74,5 @@ int main(void)
 ISR(TIMER0_OVF_vect)
 {
 	TCNT0 = 156;
-	static uint16_t tick = 0;
-	if (tick == pos) PORTB &= ~(1 << 0);
-	if (tick >= 387)
-	{
-		PORTB |= (1 << 0);
-		tick=0;
-	}
-	else tick++;
+	servo_tim0_interrupt_handler(&water_servo);
 }
